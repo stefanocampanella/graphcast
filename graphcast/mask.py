Refactor normalization and masking functions predictor wrappers (fix unmasked predictions bug in MaskedPredictor).

graphcast/mask.py
@@ -1,13 +1,15 @@
 """Wrappers for Predictors which allow working with masked data"""
-from typing import Any, Tuple
+from typing import Tuple
+
+import numpy as np
+import xarray as xr
 
 from graphcast import predictor_base
-import xarray as xr
-import numpy as np
+
 
 class MaskedPredictor(predictor_base.Predictor):
 
-  def __init__(self, predictor: predictor_base.Predictor, mask: xr.DataArray, value = np.float32(0.0)):
+  def __init__(self, predictor: predictor_base.Predictor, mask: xr.DataArray, value: np.floating | float = np.float32(0.0)):
        self._predictor = predictor
        self._value = value
        self._mask = mask
@@ -20,7 +22,7 @@ class MaskedPredictor(predictor_base.Predictor):
                ) -> xr.Dataset:
     inputs = inputs.fillna(value=self._value)
     forcings = forcings.fillna(value=self._value)
-    predictions = self._predictor(inputs, targets_template, forcings=forcings, **kwargs)
+    predictions = self._predictor(inputs=inputs, targets_template=targets_template, forcings=forcings, **kwargs)
     return predictions.where(self._mask, np.nan)
 
   def loss(self,
@@ -29,10 +31,8 @@ class MaskedPredictor(predictor_base.Predictor):
            forcings: xr.Dataset,
            **kwargs
            ) -> predictor_base.LossAndDiagnostics:
-    inputs = inputs.fillna(value=self._value)
-    forcings = forcings.fillna(value=self._value)
-    targets = targets.fillna(value=self._value)
-    return self._predictor.loss(inputs, targets, forcings=forcings, mask=self._mask, **kwargs)
+    loss_and_diagnostics, _ = self.loss_and_predictions(inputs=inputs, targets=targets, forcings=forcings, **kwargs)
+    return loss_and_diagnostics
 
   def loss_and_predictions(self,
                            inputs: xr.Dataset,
@@ -44,5 +44,10 @@ class MaskedPredictor(predictor_base.Predictor):
     inputs = inputs.fillna(value=self._value)
     forcings = forcings.fillna(value=self._value)
     targets = targets.fillna(value=self._value)
-    return self._predictor.loss_and_predictions(inputs, targets, forcings=forcings, mask=self._mask, **kwargs)
+    loss_and_diagnostics, predictions  = self._predictor.loss_and_predictions(inputs=inputs,
+                                                                              targets=targets,
+                                                                              forcings=forcings,
+                                                                              mask=self._mask,
+                                                                              **kwargs)
+    return loss_and_diagnostics, predictions.where(self._mask, np.nan)
 